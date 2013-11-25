004e35471c20db79c0f40ce69ead68e3837aa42b
Shivakumar GN <shivakumar.gn@gmail.com>
2013-11-25 09:09:52 +0530

    filepath.ToSlash() to make path work for windows as well. Update test for the fix. Run gofmt on fix_test.go.

--- fix.go
+++ fix.go
@@ -177,7 +177,7 @@ func loadPkgIndex() {
 var fset = token.NewFileSet()
 
 func loadPkg(wg *sync.WaitGroup, root, pkgrelpath string) {
-	importpath := strings.Replace(pkgrelpath, "\\", "/", -1)
+	importpath := filepath.ToSlash(pkgrelpath)
 	shortName := filepath.Base(importpath)
 
 	dir := filepath.Join(root, importpath)
