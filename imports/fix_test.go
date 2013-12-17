4c4ed48397e1ee364676f0c71c274b8aa458e9b4
Andrew Gerrand <adg@golang.org>
2013-12-17 16:40:36 +1100

    refactor into command and package

--- imports/fix_test.go
+++ imports/fix_test.go
@@ -1,4 +1,8 @@
-package main
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package imports
 
 import (
 	"bytes"
